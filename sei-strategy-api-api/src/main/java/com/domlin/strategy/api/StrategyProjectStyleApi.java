6c6934552e32966d1fdea56cbd41250885c8caaf
hejuewei <hejw@bncloud.onaliyun.com>
2023-05-11 16:34:36 +0800

    生成代码

--- sei-strategy-api-api/src/main/java/com/domlin/strategy/api/StrategyProjectStyleApi.java
+++ sei-strategy-api-api/src/main/java/com/domlin/strategy/api/StrategyProjectStyleApi.java
@@ -1,8 +1,15 @@
 package com.domlin.strategy.api;
 
 import com.changhong.sei.core.api.BaseEntityApi;
+import com.changhong.sei.core.dto.ResultData;
+import com.changhong.sei.core.dto.serach.PageResult;
+import com.changhong.sei.core.dto.serach.Search;
 import com.domlin.strategy.dto.StrategyProjectStyleDto;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 
@@ -17,4 +24,17 @@ import javax.validation.Valid;
 @FeignClient(name = "sei-strategy-api", path = StrategyProjectStyleApi.PATH)
 public interface StrategyProjectStyleApi extends BaseEntityApi<StrategyProjectStyleDto> {
     String PATH = "strategyProjectStyle";
+
+    //写一个方法，新增strategyProjectStyle
+    @PostMapping(path = "save", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation("新增strategyProjectStyle")
+    ResultData<StrategyProjectStyleDto> save(@RequestBody StrategyProjectStyleDto strategyProjectStyle);
+
+    //写一个方法查询strategyProjectStyle，根据类别名称或者code，没有条件则查询全部
+    @PostMapping(path = "findByPage", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation("分页查询strategyProjectStyle")
+    ResultData<PageResult<StrategyProjectStyleDto>> findByPage(@RequestBody Search search);
+
+
+
 }
--- sei-strategy-api-service/src/main/java/com/domlin/strategy/controller/StrategyProjectStyleController.java
+++ sei-strategy-api-service/src/main/java/com/domlin/strategy/controller/StrategyProjectStyleController.java
@@ -1,16 +1,24 @@
 package com.domlin.strategy.controller;
 
 import com.changhong.sei.core.controller.BaseEntityController;
+import com.changhong.sei.core.dto.ResultData;
+import com.changhong.sei.core.dto.serach.PageResult;
+import com.changhong.sei.core.dto.serach.Search;
+import com.changhong.sei.core.log.annotation.Log;
 import com.changhong.sei.core.service.BaseEntityService;
 import com.domlin.strategy.api.StrategyProjectStyleApi;
 import com.domlin.strategy.dto.StrategyProjectStyleDto;
 import com.domlin.strategy.entity.StrategyProjectStyle;
 import com.domlin.strategy.service.StrategyProjectStyleService;
 import io.swagger.annotations.Api;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 项目类别(StrategyProjectStyle)控制类
@@ -27,10 +35,26 @@ public class StrategyProjectStyleController extends BaseEntityController<Strateg
      */
     @Autowired
     private StrategyProjectStyleService service;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public BaseEntityService<StrategyProjectStyle> getService() {
         return service;
+    }
+
+
+    @Override
+    public ResultData<PageResult<StrategyProjectStyleDto>> findByPage(Search search) {
+        PageResult<StrategyProjectStyleDto> newPageResult = new PageResult<>();
+        List<StrategyProjectStyleDto> newRows = new ArrayList<>();
+        PageResult<StrategyProjectStyle> byPage = service.findByPage(search);
+        byPage.getRows().forEach(x -> newRows.add(modelMapper.map(x, StrategyProjectStyleDto.class)));
+        newPageResult.setRows(newRows);
+        newPageResult.setTotal(byPage.getTotal());
+        newPageResult.setPage(byPage.getPage());
+        newPageResult.setRecords(byPage.getRecords());
+        return ResultData.success(newPageResult);
     }
 
 }
